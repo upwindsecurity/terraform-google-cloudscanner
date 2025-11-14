0d6daba5d7828fae12dfba4f729c2c179e6b7c69
Dani <46299966+iDaani@users.noreply.github.com>
2025-11-14 13:13:15 +0000

    Bump default boot disk size to 40 GB for CS (#28)

--- modules/main/variables.tf
+++ modules/main/variables.tf
@@ -177,7 +177,7 @@ variable "boot_image" {
 variable "boot_disk_size_gb" {
   type        = number
   description = "The disk size in GB to use."
-  default     = 20
+  default     = 40
 }
 
 variable "boot_disk_type" {
